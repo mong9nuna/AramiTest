4fde517124fbc0f41747547a67643c01761023a7
suyul1004 <suyul1004@arambooks.com>
2023-02-17 11:03:33 +0900

    color 색상 변경

--- lib/common/const/colors.dart
+++ lib/common/const/colors.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
 
-final SAMPLE1 = Color(0xFFFFD966);
-final SAMPLE2 = Color(0xFFACD187);
 final MAIN_COLOR = Color(0xFFED7D31);
 final MAIN_COLOR_IMPACT = Color(0xFFED5F00);
+final MAIN_COLOR070 = Color(0xFFF2A46F);
+final MAIN_COLOR040 = Color(0xFFF8CBAD);
+final MAIN_COLOR_OP10 = Color(0x1AED7D31);
+final SUB_COLOR = Color(0xFF419361);
+
+final DELETE = Color(0xFFFF3434);
 
 final WHITE = Color(0xFFFFFFFF);
 final BLACK = Color(0xFF000000);
 final GRAY090 = Color(0xFF191919);
 final GRAY080 = Color(0xFF333333);
+final GRAY070 = Color(0xFF4D4D4D);
 final GRAY060 = Color(0xFF666666);
 final GRAY050 = Color(0xFF808080);
 final GRAY040 = Color(0xFF999999);
 final GRAY030 = Color(0xFFB3B3B3);
 final GRAY020 = Color(0xFFCCCCCC);
-final GRAY015 = Color(0xFFD9D9D9);
 final GRAY010 = Color(0xFFE5E5E5);
 
 final LIGHT_BG1 = Color(0xFFF9F9F9);
 final LIGHT_BG2 = Color(0xFFF1F2F3);
+final LIGHT_BG3 = Color(0xFFF8F9FB);
 
 final OP10 = Color(0x1A000000);
 final OP30 = Color(0x4D000000);
 final OP50 = Color(0x80000000);
 final OP70 = Color(0xB3000000);
 
-final OUTLINE1 = Color(0xFFDFDFDF);
+final OUTLINE = Color(0xFFDFDFDF);
