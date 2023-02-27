42cdab8e37b338496a378228b82bcf82c09eb90b
suyul1004 <suyul1004@arambooks.com>
2023-02-27 14:46:06 +0900

    Revert ".."
    This reverts commit aa0363bbfeec26a29abdbbd3c709ba2ac667ed7e.

--- lib/home/view/noti_screen.dart
+++ lib/home/view/noti_screen.dart
@@ -16,7 +16,6 @@ class _NotiScreenState extends State<NotiScreen> {
   Widget build(BuildContext context) {
     List<Widget> _views = [
       Tab(
-        height: 50.0 * getScaleWidth(context),
         child: Container(
           width: 132.0 * getScaleWidth(context),
           alignment: Alignment.center,
@@ -32,7 +31,6 @@ class _NotiScreenState extends State<NotiScreen> {
         ),
       ),
       Tab(
-        height: 50.0 * getScaleWidth(context),
         child: Container(
           width: 132.0 * getScaleWidth(context),
           alignment: Alignment.center,
